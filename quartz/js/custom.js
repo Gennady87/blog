01694a435da496d776797e264669da84fcbe046c
Gennady87 <77787779+Gennady87@users.noreply.github.com>
2024-09-24 01:50:21 +0700

    Returning to commit "theme macOS, my_sketches folder"

--- quartz/js/custom.js
+++
@@ -1,8 +0,0 @@
-var menuToggle = document.getElementById("menu-toggle");
-var sidebar = document.querySelector(".sidebar");
-
-if (menuToggle && sidebar) {
-    menuToggle.addEventListener("click", function() {
-        sidebar.classList.toggle("active");
-    });
-}
